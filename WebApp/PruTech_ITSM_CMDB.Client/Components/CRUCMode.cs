f324c22bb68a72fec170567aaf6b7adeed7199b7
Vannak Suon <vannak.suon@prudential.com.kh>
2025-11-27 17:38:58 +0700

    update

--- WebApp/PruTech_ITSM_CMDB.Client/Components/CRUCMode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PruTech_ITSM_CMDB.Client.Components;
-
-public static class CRUCModes
-{
-	public const string CREATE = "create";
-	public const string CLONE = "clone";
-	public const string READ = "read";
-	public const string UPDATE = "update";
-}
